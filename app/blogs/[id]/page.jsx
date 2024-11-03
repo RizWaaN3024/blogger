2650122b1b8bc4c6a91e2cd69f87fed1d58605ce
RizWaaN3024 <rizwaanans786@gmail.com>
2024-11-03 17:12:18 +0530

    Added Blog Page

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,6 +1,8 @@
 "use client"
 import { assets, blog_data } from '@/assets/assets';
+import Footer from '@/components/Footer';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
 const Page = ({ params }) => {
@@ -24,16 +26,57 @@ const Page = ({ params }) => {
     return ( data?<>
         <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
             <div className="flex justify-between items-center">
-                <Image src={assets.logo} width={180} alt='' className="w-[130px] sm:w-auto" />
+                <Link href={"/"}>
+                    <Image src={assets.logo} width={180} alt='' className="w-[130px] sm:w-auto" />
+                </Link>
                 <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]">
                     Get started <Image src={assets.arrow} alt='' />
                 </button>
             </div>
             <div className="text-center my-24">
-                <h1>{data.title}</h1>
+                <h1 className="text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto">{data.title}</h1>
+                <Image 
+                    src={data.author_img}
+                    className="mx-auto mt-6 border border-white rounded-full"
+                    width={60}
+                    height={60}
+                    alt=''
+                />
+                <p className="mt-1 pb-2 text-lg max-w-[740px] mx-auto">{data.author}</p>
             </div>
-
         </div>
+        <div className="mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10">
+            <Image 
+                src={data.image}
+                className="border-4 border-white"
+                width={1280}
+                height={720}
+                alt=''
+            />
+            <h1 className="my-8 text-[26px] font-semibold">Introduction</h1>
+            <p className="">{data.description}</p>
+            <h3 className="my-5 text-lg font-semibold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, nulla.</h3>
+            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corporis pariatur neque odio eius expedita itaque, libero quod ab officiis.</p>
+            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corporis pariatur neque odio eius expedita itaque, libero quod ab officiis.</p>
+            <h3 className="my-5 text-lg font-semibold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, nulla.</h3>
+            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corporis pariatur neque odio eius expedita itaque, libero quod ab officiis.</p>
+            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corporis pariatur neque odio eius expedita itaque, libero quod ab officiis.</p>
+            <h3 className="my-5 text-lg font-semibold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, nulla.</h3>
+            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corporis pariatur neque odio eius expedita itaque, libero quod ab officiis.</p>
+            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corporis pariatur neque odio eius expedita itaque, libero quod ab officiis.</p>
+            <h3 className="my-5 text-lg font-semibold">Conclusion</h3>
+            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corporis pariatur neque odio eius expedita itaque, libero quod ab officiis.</p>
+            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corporis pariatur neque odio eius expedita itaque, libero quod ab officiis.</p>
+            <div className="my-24">
+                <p className="text-black font-semibold my-4">Share this article on social media</p>
+                <div className="flex">
+                    <Image src={assets.facebook_icon} width={50} alt='' />
+                    <Image src={assets.twitter_icon} width={50} alt='' />
+                    <Image src={assets.googleplus_icon} width={50} alt='' />
+                </div>
+            </div>
+        </div>
+        <Footer />
     
     </> : <></>
     )
